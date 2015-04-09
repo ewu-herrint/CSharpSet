2f049f8e69a64daabe2b06667a1d7a20a03cbf96
tyler_herrin@eagles.ewu.edu <tyler_herrin@eagles.ewu.edu>
2015-04-09 11:10:10 -0700

    First commit.

---
+++ Set/Class1.cs
@@ -0,0 +1,73 @@
+﻿/* 
+     Name: Tyler Herrin
+     Class Description:
+*/
+
+using System;
+using System.Collections;
+
+namespace Assignment1
+{
+    public class Set
+    {
+
+        public Set()
+        {
+            // code... :)
+        }
+
+        public int Count
+        {
+            get
+            {
+                // ...
+            }
+        }
+
+        public bool Empty
+        {
+            get
+            {
+                // ...
+            }
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                // ...
+            }
+        }
+
+        public bool Contains(object o)
+        {
+            // ...
+        }
+
+        public bool Add(object o)
+        {
+            // ...
+        }
+
+        public bool Remove(object o)
+        {
+            // ...
+        }
+
+        public override bool Equals(object o)
+        {
+            // ...
+        }
+
+        public override int GetHashCode()
+        {
+            // ...
+        }
+
+        public override string ToString()
+        {
+            // ...			
+        }
+    }
+}
